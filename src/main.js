4bd497a2e28cfe142ae985e1b5f0b72291fcb7b7
LAPTOP-LB7KR35H\admin <1317819306@qq.com>
2022-07-20 14:20:26 +0800

    删除没用的东西

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ const app = new koa();
 const koaBody = require("koa-body");
 app.use(koaBody())
 const router = require("../src/router/user.route")
+
 app.listen(8080, () => {
-    // console.log('[ ctx ] >', 1)
 })
 
 app.use(router.routes())
-// const db = require('./utils/db');
-// const db=require('../src/sequelize/model/user')
